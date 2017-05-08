29becc61eac4e17f868abcc304d18cb63fe143f1
Lester G <Lester G>
2017-05-08 13:49:04 +0800

    sort inbox according to time

--- assets/controller.js
+++ assets/controller.js
@@ -297,6 +297,20 @@ app.filter('customFilter', function(){
     };
 });
 
+app.filter('orderObjectBy', function() {
+  return function(items, field, reverse) {
+    var filtered = [];
+    angular.forEach(items, function(item) {
+      filtered.push(item);
+    });
+    filtered.sort(function (a, b) {
+      return (a[field] > b[field] ? 1 : -1);
+    });
+    if(reverse) filtered.reverse();
+    return filtered;
+  };
+});
+
 
 
 
